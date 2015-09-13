33e606fa795697b0088aa0094fc0169cb947c38a
marvinhinkley <hinkley.marvin@gmail.com>
2015-09-13 11:59:17 -0700

    added auth configuration to gitignore

--- config/auth.js
+++ config/auth.js
@@ -1,12 +1,12 @@
 module.exports = {
     'facebookAuth' : {
-        'clientID'      : '846990962081615',
-        'clientSecret'  : '025e4a0c761664ad0446ad5c63827196', // your App Secret
-        'callbackURL'   : 'http://localhost:9600/auth/facebook/callback'
+        'clientID'      : '',
+        'clientSecret'  : '',
+        'callbackURL'   : ''
     },
     'googleAuth' : {
-        'clientID'      : 'your-secret-clientID-here',
-        'clientSecret'  : 'your-client-secret-here',
-        'callbackURL'   : 'http://localhost:9600/auth/google/callback'
+        'clientID'      : '',
+        'clientSecret'  : '',
+        'callbackURL'   : ''
     }
 };
